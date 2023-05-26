ce10aa179822c2900d0b1d78dbef4a60edb06e93
sonamkolhekar <sonamkolhekar@gmail.com>
2023-05-26 14:53:22 +0530

    first commit

--- src/com/demo/Addition.java
+++ src/com/demo/Addition.java
@@ -13,6 +13,11 @@ public class Addition {
 		x = 40;
 		y = 40;
 		System.out.println("Addition is " + (x + y));
+		//add data from elcips
+		x = 50;
+		y = 50;
+		System.out.println("Addition is " + (x + y));
+		
 
 	}
 
