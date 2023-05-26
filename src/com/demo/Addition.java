39208fdcb03ad7d35c6078c24768516fc8c36eb3
sonamkolhekar <sonamkolhekar@gmail.com>
2023-05-26 14:46:07 +0530

    add the data

--- src/com/demo/Addition.java
+++ src/com/demo/Addition.java
@@ -3,14 +3,13 @@ package com.demo;
 public class Addition {
 
 	public static void main(String[] args) {
-		int x=20;
-		int y=20;
-		System.out.println("Addition is "+(x+y));
-		
-		
-		
-		
-		
+		int x = 20;
+		int y = 20;
+		System.out.println("Addition is " + (x + y));
+		x = 30;
+		y = 30;
+		System.out.println("Addition is " + (x + y));
+
 	}
 
 }
