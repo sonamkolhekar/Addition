0e71e0701102783b8db8b03c10965920d62966fc
sonamkolhekar <sonamkolhekar@gmail.com>
2023-05-26 14:39:27 +0530

    line add

---
+++ src/com/demo/Addition.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int x=20;
+		int y=20;
+		System.out.println("Addition is "+(x+y));
+		
+		
+		
+		
+		
+	}
+
+}
